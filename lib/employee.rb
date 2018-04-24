7493d1e775e588ec3a8e62b43592e2f584332970
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-24 21:17:04 +0000

    added exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,17 @@
+require 'securerandom'
+
 class Employee < ActiveRecord::Base
   belongs_to :store
 
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates_associated :store
+
+  before_create :generate_password
+
+  private
+    def generate_password
+      self.password = SecureRandom.urlsafe_base64(6)
+      puts "Generated password #{self.password}"
+    end
 end
