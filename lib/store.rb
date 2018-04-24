8ab88d8818d35ffa69339695de7cf4ff695472fb
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-24 21:23:09 +0000

    completed exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -6,6 +6,8 @@ class Store < ActiveRecord::Base
 
   validate :must_carry_men_and_or_womens_wear
 
+  before_destroy :cannot_destroy_empty_store
+
   def must_carry_men_and_or_womens_wear
     unless mens_apparel || womens_apparel
       errors.add(:mens_apparel, "Must sell at least one of mens_apparel or womens_apparel")
@@ -13,4 +15,11 @@ class Store < ActiveRecord::Base
     end
 
   end
+
+  private
+    def cannot_destroy_empty_store
+      if self.employees.count > 0
+        throw :abort
+      end
+    end
 end
